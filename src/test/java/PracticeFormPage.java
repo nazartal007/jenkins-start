bcc250cf87a19ea377ed187192ff435958657944
nazartal007 <a.nazarov@robo.finance>
2021-06-16 22:22:51 +0700

    added steps

--- src/test/java/PracticeFormPage.java
+++ src/test/java/PracticeFormPage.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -30,43 +31,52 @@ public class PracticeFormPage {
             stateAndCityModal = $x("//*[@class='table-responsive']//*[text()='State and City']/following::td"),
             studentEmailModal = $x("//*[@class='table-responsive']//*[text()='Student Email']/following::td");
 
+    @Step("открываем страницу с форомй для заполнения")
     public void openFormPage() {
         open("https://demoqa.com/automation-practice-form");
     }
 
+    @Step("Заплоняем имя студента даннымы {firstName} {lastName}")
     public void setNameStudent(String firstName, String lastName) {
         firstNameStudent.setValue(firstName);
         lastNameStudent.setValue(lastName);
     }
 
+    @Step("Указываем адрес эдектронной почты: {userEmail}")
     public void setEmail(String userEmail) {
         email.setValue(userEmail);
     }
 
+    @Step("Выбираем мужской пол")
     public void chooseGenderMale() {
         genderMale.scrollTo().click();
 
     }
 
+    @Step("Вводим номер телефона: {number}")
     public void setMobileNumber(String number) {
         telNumberInput.setValue(number);
     }
 
+    @Step("Выбираем предмет: {name}")
     public void chooseSubject(String name) {
         subjectsInput.setValue(name);
         $$(byText(name)).get(1).click();
     }
 
+    @Step("Указываем все хобби")
     public void chooseHobbies() {
         hobbiesSports.click();
         hobbiesReading.click();
         hobbiesMusic.click();
     }
 
+    @Step("Указываем текущий адресс: {address}")
     public void setCurrentAddress(String address) {
         currentAddress.setValue(address);
     }
 
+    @Step("Выбираем штат {stateName} и город {cityName}")
     public void choseStateAndCity(String stateName, String cityName) {
         stateField.click();
         $(byText(stateName)).click();
@@ -74,44 +84,49 @@ public class PracticeFormPage {
         $(byText(cityName)).click();
     }
 
+    @Step("Нажимаем отправить")
     public void submitForm() {
         submitButton.click();
     }
 
+    @Step("Проверяем что имя и фамилия равны: {firstName} {lastName}")
     public void assertStudentFullName(String firstName, String lastName) {
         studentNameModal.shouldHave(text(firstName + " " + lastName));
     }
 
+    @Step("Проверяем что электронная почта равна {email}")
     public void assertStudentEmail(String email) {
         studentEmailModal.shouldHave(text(email));
     }
 
+    @Step("Проверяем, что пол выбран {gender}")
     public void assertGender(String gender) {
         genderModal.shouldHave(text(gender));
     }
 
+    @Step("Номер телефона в модалке равен {number}")
     public void assertMobNumber(String number) {
         phoneModal.shouldHave(text(number));
     }
 
-    public void assertDateBird(String dateBird) {
-        dateOfBirdModal.shouldHave(text(dateBird));
-    }
-
+    @Step("Предмет равен {subject}")
     public void assertSubject(String subject) {
         subjectsModal.shouldHave(text(subject));
     }
 
+    @Step("Хобби указаны {hobbies}")
     public void assertHobbies(String ...hobbies) {
         for (String oneHobbies : hobbies) {
             hobbiesModal.shouldHave(text(oneHobbies));
         }
     }
 
+    @Step("Текущий адрес указан {address}")
     public void assertCurrentAddress(String address) {
         addressModal.shouldHave(text(address));
     }
 
+    @Step("Город и штат указаны {state} {city}")
     public void assertStateAndCity(String state, String city) {
         stateAndCityModal.shouldHave(text(state + " " + city));
     }
