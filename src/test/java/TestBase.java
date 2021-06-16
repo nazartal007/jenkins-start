7e575065e3d94d4bc89574b12518974583291621
nazartal007 <a.nazarov@robo.finance>
2021-06-16 21:37:30 +0700

    added close webdriver

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,8 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 public class TestBase {
@@ -7,5 +10,10 @@ public class TestBase {
     static void setup(){
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterAll
+    public void tearDown(){
+        closeWebDriver();
     }
 }
