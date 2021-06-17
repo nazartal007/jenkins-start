c0fceaccb30644b63c0db3e9a10e24146ea5469a
nazartal007 <a.nazarov@robo.finance>
2021-06-17 16:15:32 +0700

    added video

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -5,7 +5,6 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -41,5 +40,42 @@ public class Attach {
         );
     }
 
+    //    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+//    public static String addVideo(String sessionId) {
+//        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+//                + getVideoUrl(sessionId)
+//                + "' type='video/mp4'></video></body></html>";
+//    }
+//
+    public static void addVideo(String sessionId) {
+        URL videoUrl = getVideoUrl(sessionId);
+        if (videoUrl != null) {
+            InputStream videoInputStream = null;
+            sleep(1000);
+
+            for (int i = 0; i < 10; i++) {
+                try {
+                    videoInputStream = videoUrl.openStream();
+                    break;
+                } catch (FileNotFoundException e) {
+                    sleep(1000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
+        }
+    }
+
+    public static URL getVideoUrl(String sessionId) {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
+
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,9 +5,13 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 
 public class TestBase {
@@ -16,14 +20,26 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.startMaximized = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
-    @AfterAll
-    static void tearDown() {
+    @AfterEach
+    public void tearDown() {
+        String sessionId = getSessionId();
+
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         closeWebDriver();
+
+        Attach.addVideo(sessionId);
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
