9787002e50eee76ba51c6b8b6cb891b7b0ba200c
nazartal007 <a.nazarov@robo.finance>
2021-06-26 19:37:14 +0700

    added something

---
+++ src/test/java/tests/OwnerTests.java
@@ -0,0 +1,22 @@
+package tests;
+
+import config.Credentials;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static config.Credentials.credentials;
+
+public class OwnerTests {
+    @Test
+    @Tag("owner")
+    public void ownerTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = String.format("I try to login as %s with password %s", login, password);
+        System.out.println(message);
+    }
+}
